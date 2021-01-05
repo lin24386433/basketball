e654329253b134d464c3de47ccef85cca2bcc186
cumolin <wilson24386433@gmail.com>
2021-01-05 21:30:25 +0800

    no message

--- js/intro.js
+++ js/intro.js
@@ -33,13 +33,16 @@ function processFormData() {
 }
 
 function getData() {
-    introComment.orderBy("time", "desc");
-    introComment.get().then(querySnapshot => {
+    var commentCount = 0;
+    introComment.orderBy("time", "desc").get().then(querySnapshot => {
         querySnapshot.forEach(doc => {
+            commentCount = commentCount + 1;
+            document.getElementById('allcomments').innerHTML = "所有留言(" + commentCount + ")";
             dataName = doc.data()['name'];
             dataComment = doc.data()['comment'];
-            commentBox.innerHTML += "<div style='display: flex;'><img src='images/basketball-ball.png' style='width: 50px; height:50px; margin-left: 10px;'><div class='comment-reuse' style='margin-left: 10px; margin-bottom: 30px;'><p style='font-weight: bold;'>" + dataName + "</p><br><p>" + dataComment + "</p></div></div> <div style='width:100%; height:1px; background-color: #000; margin-bottom: 15px;'></div>";
+            commentBox.innerHTML += "<div style='display: flex;'><img src='images/basketball-ball.png' style='width: 50px; height:50px; margin-left: 10px;'><div class='comment-reuse' style='margin-left: 15px; margin-bottom: 30px;'><p style='font-weight: bold; font-size: 18px;'>" + dataName + ' 說：' + "</p><br><p>" + dataComment + "</p></div></div> <div style='width:100%; height:1px; background-color: #000; margin-bottom: 15px;'></div>";
         });
     });;
+    
 }
 
