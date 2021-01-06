2ae5991f53e1bb50318e221e8f42eaa31d2f8732
cumolin <wilson24386433@gmail.com>
2021-01-06 20:55:48 +0800

    no message

--- js/training.js
+++ js/training.js
@@ -41,6 +41,8 @@ function getData() {
             document.getElementById('allcomments').innerHTML = "所有留言(" + commentCount + ")";
             dataName = doc.data()['name'];
             dataComment = doc.data()['comment'];
+            dataComment = dataComment.replace(/  /g, "  ");
+            dataComment = dataComment.replace(/\n/g,"<br>");
             commentBox.innerHTML += "<div style='display: flex;'><img src='images/basketball-ball.png' style='width: 50px; height:50px; margin-left: 10px;'><div class='comment-reuse' style='margin-left: 15px; margin-bottom: 30px;'><p style='font-weight: bold; font-size: 18px;'>" + dataName + ' 說：' + "</p><br><p>" + dataComment + "</p></div></div> <div style='width:100%; height:1px; background-color: #000; margin-bottom: 15px;'></div>";
         });
     });;
